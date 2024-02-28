c9924d4ae77b81743d16f2efd3471ea9cb2a5de8
Dzung Nguyen <bluesky.1289@gmail.com>
2024-02-28 22:40:36 +0700

    release v0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Skema.MixProject do
   def project do
     [
       app: :skema,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
